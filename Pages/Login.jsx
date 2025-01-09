749041bd14ed27c5cb00d1e1ef92b310c9167c93
tomhabesha <temesgenwondim@gmail.com>
2025-01-09 17:49:53 +0300

    the commit of the front end when i do the login page

--- Pages/Login.jsx
+++ Pages/Login.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
-
+import { useRef } from 'react'
+import axios from '../src/axiosConfig';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
+
+     const navigate = useNavigate()
+    const emailDom = useRef();
+    const passwordDome = useRef();
+    
+      async function handleSubmit(e) {
+        e.preventDefault();
+        const emailvalue = emailDom.current.value;
+        const passwordvalue = passwordDome.current.value;
+      const ismach = !passwordvalue
+      console.log(ismach)
+        if(!emailvalue || !passwordvalue){
+             alert('please provide all required information');
+             return;
+        }
+           
+        try {
+            const {data} = await axios.post('/user/login',{
+                email:emailvalue,
+                password:passwordvalue
+            })
+            alert('login successfully')
+            localStorage.setItem('token',data.token)
+            // navigate('/')
+            console.log(data);
+        } catch (error) {
+            alert(error?.response?.data?.msg)
+            console.log(error.response.data)
+        }
+
+    }
   return (
     <div>
-      login
+      <form onSubmit={handleSubmit}> 
+        <div>
+          <input ref={passwordDome} type="password" placeholder='password'/>
+        </div>
+        <br /><br />
+        <div>
+          <input ref={emailDom} type="email" placeholder='email'/>
+        </div>
+        <br /><br />
+        <button type='submit'>Login</button>
+      </form>
     </div>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Home from '../Pages/Home'
 import Login from '../Pages/Login'
 import Register from '../Pages/Register'
+import axios from './axiosConfig'
 
 const App = () => {
+
+  const Navigate = useNavigate
+  async function checkuser(){
+     try {
+      await axios.get('/user/check')
+     } catch (error) {
+      console.log(error.response);
+      Navigate('/login')
+     }
+  }
+
+  useEffect(()=>{
+    checkuser();
+  },[])
+
+
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
